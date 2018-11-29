ea9d0184220682103b7f5208a63fbaded97ee31f
aaawqy <1902412998@qq.com>
2018-11-29 09:42:45 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:wqy
+ * createTime:2018-11-29 09:06
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub提交");
+    }
+}
