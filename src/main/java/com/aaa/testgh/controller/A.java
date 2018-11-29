9c324351d60cb8d0dcb0f3f7e0eb70f28fab2741
aaawqy <1902412998@qq.com>
2018-11-29 10:48:00 +0800

    在Dev下加了打印

--- src/main/java/com/aaa/testgh/controller/A.java
+++ src/main/java/com/aaa/testgh/controller/A.java
@@ -9,5 +9,6 @@ package com.aaa.testgh.controller;
 public class A {
     public static void main(String[] args) {
         System.out.println("1111");
+        System.out.println("测试合并分支");
     }
 }
--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -9,5 +9,6 @@ package com.aaa.testgh.controller;
 public class DeptController {
     public static void main(String[] args) {
         System.out.println("测试GitHub提交");
+        System.out.println("测试合并分支");
     }
 }
